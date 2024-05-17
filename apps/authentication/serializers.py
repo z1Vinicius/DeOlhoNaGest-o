f0091a3415156144e7daa02fb8f89aeac1c52a10
Vinícius <viniciusara2013@gmail.com>
2024-05-16 21:55:53 -0300

    :bug: Fixed register serializer

--- apps/authentication/serializers.py
+++ apps/authentication/serializers.py
@@ -3,26 +3,26 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    fullname = serializers.CharField(write_only=True)
+    fullName = serializers.CharField(write_only=True)
     password = serializers.CharField(write_only=True)
     confirmPassword = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'fullname', 'email', 'password', 'confirmPassword')
+        fields = ('username', 'fullName', 'email', 'password', 'confirmPassword')
 
     def validate(self, data):
         if data['password'] != data['confirmPassword']:
             raise serializers.ValidationError("As senhas não coincidem.")
-        if len(data['fullname'].split()) < 2:
+        if len(data['fullName'].split()) < 2:
             raise serializers.ValidationError("Por favor, digite seu nome completo (nome e sobrenome).")
         return data
 
     def create(self, validated_data):
-        fullname = validated_data.pop('fullname')
+        fullName = validated_data.pop('fullName')
         password = validated_data.pop('password')
         validated_data.pop('confirmPassword')
-        first_name, last_name = fullname.split(' ', 1)
+        first_name, last_name = fullName.split(' ', 1)
         user = User(
             username=validated_data['username'],
             email=validated_data['email'],
